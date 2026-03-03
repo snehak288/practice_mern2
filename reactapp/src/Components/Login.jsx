c944bfbd5975ce1b5873d4183c3a3f858fa3eedf
snehak288 <snehakpts@gmail.com>
2026-03-04 00:36:59 +0530

    routers added

---
+++ reactapp/src/Components/Login.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react'
+
+function Login() { const[formData,setFormData]=useState({
+        userName:"",
+        password:""
+    })
+    const[errors,setErrors]=useState({})
+
+    const validation=()=>{
+        let validationErrors={};
+        if (!formData.userName){
+            validationErrors.userName="Username is required"
+        }
+        if(!formData.password){
+            validationErrors.password="Password is required"
+        }
+        return validationErrors
+    }
+    const handleSubmit=async(e)=>{
+        e.preventDefault();
+        const validationErrors=validation()
+        if(Object.keys(validationErrors).length>0){
+            setErrors(validationErrors)
+            return
+        }
+        setErrors({})
+        const response=await axios.post('http:localhost:8080/users/login',formData)
+        console.log(response.data)
+    }
+  return (
+    <div>
+        <form onSubmit={handleSubmit} noValidate>
+            <input type='text' placeholder='username' value={formData.userName} onChange={(e)=>{if(e.target.value){setErrors({...errors,userName:""})};setFormData({...formData,userName:e.target.value})}}/>
+            <input type='text' placeholder='password' value={formData.password} onChange={(e)=>{if(e.target.value){setErrors({...errors,password:""})};setFormData({...formData,password:e.target.value})}}/>
+        </form>
+    </div>
+  )
+}
+
+export default Login
